624d97ba30f5fef719b79445f5980e6f86a657c3
Dobr <dobromir07@yandex.ru>
2025-01-16 19:08:18 +0500

    +generarate a index file

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,10 +1,14 @@
 import { trpc } from '../lib/trpc'
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 import { getPrimerTrpcRoute } from './getprimer'
 import { getPrimeryTrpcRoute } from './getprimery'
+// @endindex
 
 export const trpcRouter = trpc.router({
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
   getPrimer: getPrimerTrpcRoute,
   getPrimery: getPrimeryTrpcRoute,
+  // @endindex
 })
 
 export type TrpcRouter = typeof trpcRouter
