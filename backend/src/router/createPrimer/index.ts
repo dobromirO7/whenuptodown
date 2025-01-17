fd192bef26125dfb2729ebd1a32a54fb1c178a56
Dobr <dobromir07@yandex.ru>
2025-01-17 22:30:29 +0500

    +error of form nick field

--- backend/src/router/createPrimer/index.ts
+++ backend/src/router/createPrimer/index.ts
@@ -3,6 +3,9 @@ import { trpc } from '../../lib/trpc'
 import { zCreatePrimerTrpcInput } from './input'
 
 export const createPrimerTrpcRoute = trpc.procedure.input(zCreatePrimerTrpcInput).mutation(({ input }) => {
+  if (primery.find((primer) => primer.nick === input.nick)) {
+    throw Error('Primer with this nick already exists')
+  }
   primery.unshift(input)
   return true
 })
--- webapp/src/pages/whenuptodown/NewPrimerPage/index.tsx
+++ webapp/src/pages/whenuptodown/NewPrimerPage/index.tsx
@@ -9,6 +9,7 @@ import { trpc } from '../../../lib/trpc'
 
 export const NewPrimerPage = () => {
   const [successMessageVisible, setSuccessMessageVisible] = useState(false)
+  const [submittingError, setSubmittingError] = useState<string | null>(null)
   const createPrimer = trpc.createPrimer.useMutation()
   const formik = useFormik({
     initialValues: {
@@ -18,20 +19,25 @@ export const NewPrimerPage = () => {
       text: '',
     },
     validate: withZodSchema(zCreatePrimerTrpcInput),
-
     onSubmit: async (values) => {
-      await createPrimer.mutateAsync(values)
-      formik.resetForm()
-      setSuccessMessageVisible(true)
-      setTimeout(() => {
-        setSuccessMessageVisible(false)
-      }, 3000)
+      try {
+        await createPrimer.mutateAsync(values)
+        formik.resetForm()
+        setSuccessMessageVisible(true)
+        setTimeout(() => {
+          setSuccessMessageVisible(false)
+        }, 3000)
+      } catch (error: any) {
+        setSubmittingError(error.message)
+        setTimeout(() => {
+          setSubmittingError(null)
+        }, 3000)
+      }
     },
   })
 
   return (
     <Segment title="New Primer">
-      {/* Это компонент  */}
       <form
         onSubmit={(e) => {
           e.preventDefault()
@@ -43,6 +49,7 @@ export const NewPrimerPage = () => {
         <Input name="description" label="Description" formik={formik} />
         <Textarea name="text" label="Text" formik={formik} />
         {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
+        {!!submittingError && <div style={{ color: 'red' }}>{submittingError}</div>}
         {successMessageVisible && <div style={{ color: 'green' }}>Primer created successfully</div>}
         <button type="submit">
           Create Primer
